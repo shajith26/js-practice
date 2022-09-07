6e8056c032f05390c73965373bc747ac8875d3d6
shajith26 <mohamedshajith321@gmail.com>
2022-09-07 11:01:56 +0530

    new-file

---
+++ js-obj/object.js
@@ -0,0 +1,92 @@
+// Object 
+{
+	let person={
+		name: "rocky",
+		age: 30,
+		gender: "male",
+		address: "india"
+	}
+	console.log(person)
+	console.log(person.name)
+}
+{
+	let wear={
+		name: "otto",
+		materials:"shirt,t-shirt,phant",
+		address: "Tenkasi"
+	}
+	console.log(wear)
+	console.log(wear.name)
+}
+{
+	let bike={
+		model:"KTM rc 390",
+		color:"orange and white"
+	}
+	console.log(bike)
+	console.log(bike.model)
+}
+{
+	let mobile={
+		model:"samsung f62",
+		battery:"inbuild",
+		camera:"48 pixel",
+		display:"super amoled display"
+	}
+	console.log(mobile)
+	console.log(mobile.model)
+}
+{
+	let note={
+		brand:"classmate",
+		pages:"180 pages",
+		type:"unruled"
+	}
+	console.log(note)
+	console.log(note.brand)
+}
+{
+	let school={
+		name:"bharath",
+		place:"ilanji",
+		sylabus:"state board and CBSE"
+	}
+	console.log(school)
+	console.log(school.name)
+}
+{
+	let pen={
+		brand:"skilly",
+		color:"blue",
+		type:"plastic"
+	}
+	console.log(pen)
+	console.log(pen.brand)
+}
+{
+	let bag={
+		brand:"sky bag",
+		water_resistance: true,
+		color:"blue"
+	}
+	console.log(bag)
+	console.log(bag.brand)
+}
+{
+	let laptop={
+		brand:"lenova",
+		model:"IdeaPad S145",
+		color:"grey"
+	}
+	console.log(laptop)
+	console.log(laptop.brand)
+}
+{
+	let headset={
+		brand:"boat",
+		color:"red",
+		type:"type c"
+	}
+	console.log(headset)
+	console.log(headset.brand)
+}
